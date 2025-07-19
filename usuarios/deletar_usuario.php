ca394b896816133713a98656201c60c6188003ee
raphanunes443 <raphaelnunes443@gmail.com>
2025-07-18 21:53:52 -0300

    Add files via upload

---
+++ usuarios/deletar_usuario.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Casa e Sabor!</title>
+</head>
+<body>
+
+<!--Nessa pagina o usuario ira deletar o seu cadastro-->
+
+<!--NAVBAR-->
+
+<!--VALIDAR SESSÃO -->
+
+<!--FORMULARIO PARA DELETAR CADASTRO-->
+
+<!-- CONEXÃO BANCO -->
+
+<!-- FOOTER -->
+
+</body>
+</html>
+
