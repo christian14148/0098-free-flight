27289f19c58f9d2897eaef545520e707550cdb05
Raphael Nunes Souza <1059953@senacminas.edu.br>
2025-07-22 21:49:26 -0300

    Fiz uma parte do login

--- sessao/login.php
+++ sessao/login.php
@@ -1,11 +1,39 @@
 <?php
-session_start();
 
-if(isset($_SESSION['cpf_usuario'])){
-header("Location:login.php");
-exit();
-}
+    include('../banco/conexao.php');
+    if (isset($_POST['envio'])){
+        $login = $_POST['login'];
+        $senha = hash('md2',$_POST['senha']);
+
+            $resultado = $conn->query("SELECT * FROM usuarios WHERE cpf='$login'");
+            $usuario = $resultado->fetch_assoc();
+
+        if(($login == $usuario['cpf']) && ($senha == $usuario['senha_usuario'])){
+            $_SESSION['login'] = 1;
+            header('location: ../index.html');
+        }else{
+            $_SESSION['login'] = 0;
+            echo "<div>Login ou senha não confere. Tente novamente.</div>";
+        }
+    }
 ?>
-<h2>seja bem vindo</h2>
-<p> voce esta logado com cpf: <?php echo $_SESSION['cpf_usuario'];?></p>
-<a href="logout.php">Sair</a>
+
+
+<h2>Faça seu login</h2>
+<form action="login.php" method="POST">
+    <div class="mb-3 row">
+        <label >CPF </label>
+        <div >
+            <input type="text" class="form-control" id="login" name="login" required>
+        </div>
+    </div>
+    <div>
+        <label>Senha</label>
+        <div>
+            <input type="password" class="form-control" id="senha" name="senha" required>
+        </div>
+    </div>
+    
+    <button type="submit" class="btn btn-primary" name="envio">Login</button>
+    <a href="../usuarios/cadastrar_usuario.php">Cancelar</a>
+</form>
