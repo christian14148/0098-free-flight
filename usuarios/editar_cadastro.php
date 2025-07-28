fbca73da1ede1eed2924c267dd39c7f17fc01d6a
Raphael Nunes Souza <1059953@senacminas.edu.br>
2025-07-28 19:12:25 -0300

    CEP adicionado e API para localização também

--- usuarios/editar_cadastro.php
+++ usuarios/editar_cadastro.php
@@ -138,6 +138,12 @@ if ($_GET['p']=='editar'){
         </div>
     </div>
     <div>
+        <label>CEP </label>
+        <div>
+            <input type="text" id="cep" name="cep" value="<?php echo $contato['cep'];?>" onblur="pesquisacep(this.value);" maxlength="8">
+        </div>
+    </div>
+    <div>
         <label>Rua </label>
         <div>
             <input type="text" id="rua" name="rua" value="<?php echo $contato['rua'];?>">
@@ -175,6 +181,74 @@ if ($_GET['p']=='editar'){
     </div>
     <button type="submit">Salvar</button>
 </form>
+
+    <script>
+function pesquisacep(valor) {
+
+    // Nova variável "cep" somente com dígitos.
+    var cep = valor.replace(/\D/g, '');
+
+    // Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+        // Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+
+        // Valida o formato do CEP.
+        if (validacep.test(cep)) {
+
+            // Preenche os campos com "..." enquanto consulta webservice.
+            document.getElementById('rua').value = "...";
+            document.getElementById('bairro').value = "...";
+            document.getElementById('cidade').value = "...";
+            document.getElementById('uf').value = "...";
+
+            // Cria um elemento javascript.
+            var script = document.createElement('script');
+
+            // Sincroniza com o callback.
+            script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback';
+
+            // Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
+
+        } //end if.
+        else {
+            //cep é inválido.
+            alert("Formato de CEP inválido");
+            limpa_formulario_cep();
+            
+        }
+    } // end if.
+    else {
+        // cep sem valor, limpa formulário.
+        limpa_formulario_cep();
+    }
+}
+
+function limpa_formulario_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('rua').value = ("");
+    document.getElementById('bairro').value = ("");
+    document.getElementById('cidade').value = ("");
+    document.getElementById('uf').value = ("");
+}
+
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        // Atualiza os campos com os valores.
+        document.getElementById('rua').value = (conteudo.logradouro);
+        document.getElementById('bairro').value = (conteudo.bairro);
+        document.getElementById('cidade').value = (conteudo.localidade);
+        document.getElementById('uf').value = (conteudo.uf);
+    } //end if.
+    else {
+        // CEP não Encontrado.
+        limpa_formulario_cep();
+        alert("CEP não encontrado.");
+    }
+}
+</script>
 
 </body>
 </html>
